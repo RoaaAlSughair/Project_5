e5b52b4d3de5f43bcf72894b8bfc138b277ea7e6
C05_Basem <basemhmoud735@gmail.com>
2021-06-23 23:44:19 +0300

    create table  called book

---
+++ backend/db/test1.sql
@@ -0,0 +1,17 @@
+CREATE TABLE book(
+    book_id INT AUTO_INCREMENT NOT NULL,
+    img_book image,
+    title NVARCHAR(50),
+    description NVARCHAR(255),
+    author NVARCHAR(255),
+    publisher NVARCHAR(100),
+    edition INT(5),
+    pages INT(5),
+    price float,
+    author NVARCHAR(100),
+    is_avalible TINYINT DEFAULT 1,
+    comment_id INT,
+    FOREIGN KEY (comment_id) REFERENCES comments(comment_id),
+    is_deleted TINYINT DEFAULT 0,
+    PRIMARY KEY (book_id)
+);
