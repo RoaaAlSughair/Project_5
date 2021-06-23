c5eb44442aa2466eab12a57b134ff089a4c19e36
Heba_Alshiyyab <81535776+HebaShiyyab@users.noreply.github.com>
2021-06-23 23:30:03 +0300

    users table

--- backend/db/test.sql
+++ backend/db/test.sql
@@ -6,3 +6,14 @@ CREATE TABLE roles (
     PRIMARY KEY (role_id)
 );
 
+CREATE TABLE users(
+    user_id INT AUTO_INCREMENT NOT NULL,
+    firstName nVARCHAR(255),
+    lastName nVARCHAR(255),
+    email nVARCHAR(255),
+    password nVARCHAR(255),
+    role_id INT,
+    FOREIGN KEY (role_id) REFERENCES roles(role_id),
+    is_deleted TINYINT DEFAULT 0,
+    PRIMARY KEY (user_id)
+);
