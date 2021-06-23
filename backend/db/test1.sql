68e0551e3b1c2d5923e6aed4ec7cf50a49092f67
C05_Basem <basemhmoud735@gmail.com>
2021-06-24 00:04:40 +0300

    create table cart

---
+++ backend/db/test1.sql
@@ -0,0 +1,29 @@
+CREATE TABLE book(
+    book_id INT AUTO_INCREMENT NOT NULL,
+    img_book image,
+    title NVARCHAR(50),
+    description NVARCHAR(255),
+    author NVARCHAR(255),
+    publisher NVARCHAR(100),
+    edition INT(5),
+    pages INT(5),
+    price float,
+    author NVARCHAR(100),
+    is_avalible TINYINT DEFAULT 1,
+    comment_id INT,
+    FOREIGN KEY (comment_id) REFERENCES comments(comment_id),
+    is_deleted TINYINT DEFAULT 0,
+    PRIMARY KEY (book_id)
+);
+
+ALTER TABLE comments
+ADD FOREIGN KEY (book_id) REFERENCES book(book_id);
+
+CREATE TABLE cart(
+    id INT AUTO_INCREMENT NOT NULL,
+    quantity INT,
+    FOREIGN KEY (book_id) REFERENCES book(book_id),
+    total price float,
+    is_deleted TINYINT DEFAULT 0,
+    PRIMARY KEY (id) FOREIGN KEY (user_id) REFERENCES users(id),
+);
