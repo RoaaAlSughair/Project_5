2a883f1a09c408048964378da137d7f1c077b9c9
Heba_Alshiyyab <81535776+HebaShiyyab@users.noreply.github.com>
2021-06-25 01:24:49 +0300

    register

--- backend/routers/controllers/auth/signUp.js
+++ backend/routers/controllers/auth/signUp.js
@@ -1,1 +1,18 @@
-const express = require("express");
+
+const connection =require('./../../../db/db');
+
+
+const Register = (req, res) => {
+    const query = `INSERT INTO users (firstName, lastName, email, password) VALUES (?, ?, ?,?)`;
+    const data = [req.body.firstName, req.body.lastName, req.body.email,req.body.password ] 
+
+    connection.query(query, data, (err, results) => {
+		if(err) throw err;
+	res.json(results);
+
+	  });
+  };
+  
+  module.exports = {
+    Register,
+  };
--- backend/routers/routes/auth/signUp.js
+++ backend/routers/routes/auth/signUp.js
@@ -1,1 +1,11 @@
 const express = require("express");
+
+const {Register} = require('./../../controllers/auth/signUp');
+
+const registerRouter = express.Router();
+
+registerRouter.post('/Register', Register);
+
+module.exports = registerRouter;
+
+
