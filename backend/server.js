96f11b056dd330fdb2214e28eb58bbc6bdd5fabe
Heba_Alshiyyab <81535776+HebaShiyyab@users.noreply.github.com>
2021-06-27 21:42:27 +0300

    all my  tiket in backend is done

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,21 @@
 const express = require('express');
 const cors = require('cors');
-require('dotenv').config();
 const db = require('./db/db');
-
-
+​
 const app = express();
-
+​
 //routers
-const registerRouter = require('./routers/routes/auth/signUp')
-const loginRouter = require('./routers/routes/auth/login');
+​
 //built-in middlewares
 app.use(express.json());
-
+​
 //third-party middleware
 app.use(cors());
-
+​
 //app routers
-app.use(loginRouter);
-app.use(registerRouter);
+​
 const PORT = process.env.PORT || 5000;
-
+​
 app.listen(PORT, () => {
 	console.log(`Server On ${PORT}`);
 });
