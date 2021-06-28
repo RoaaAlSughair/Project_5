b1e9295a1ff0551f4f92e62512eda4e0243d7a7f
RuaaAlsugheir <alsugheirruaa@gmail.com>
2021-06-28 21:28:00 +0300

    fixing an error that happened while merging

--- backend/routers/controllers/books.js
+++ backend/routers/controllers/books.js
@@ -1,52 +1,24 @@
-const getBookByTitle = (req, res) => {
-  // get the book's title
-  const title = req.query.title;
+const connection = require("../../db/db");
 
-  // inject the title in the query order to get the result
-  const data = [title];
-  const query = `SELECT * FROM book WHERE title = ?;`;
-  connection.query(query, data, (err, result) => {
-    if (err) {
-      throw err;
-    }
-    res.status(200).json(result);
-  });
-};
-
-const getBooksByAuthor = (req, res) => {
-  // get the author
-  const author = req.query.author;
-
-  // inject the author in the query order to get the result
-  const data = [author];
-  const query = `SELECT * FROM book WHERE author = ?;`;
-  connection.query(query, data, (err, result) => {
-    if (err) {
-      throw err;
-    }
-    res.status(200).json(result);
-  });
-};
-
-// export functions
-module.exports = {
-  getAllBooks,
-  getBookByTitle,
-  getBooksByAuthor,
-=======
-const getBooksByCategory = (req, res) => {
-  const category = req.params.category_id;
-  const query = `SELECT * FROM book WHERE category_id=${category}`;
+const getAllBooks = (req, res) => {
+  const query = `SELECT * FROM book`;
   db.query(query, (err, results) => {
     if (err) throw err;
     res.status(201).json(results);
   });
 };
-const deleteBooksByID= (req, res) => {
-  
+
+const getBooksByCategory = (req, res) => {
+  const category = req.params.category_id;
+  const query = `SELECT * FROM book WHERE category_id=${category}`;
+  connection.query(query, (err, results) => {
+    if (err) throw err;
+    res.status(201).json(results);
+  });
+};
+const deleteBooksByID = (req, res) => {
   const book_id = req.params.book_id;
 
- 
   const query = `DELETE FROM book WHERE book_id = "${book_id}";`;
   db.query(query, (err, result) => {
     if (err) {
@@ -56,10 +28,28 @@ const deleteBooksByID= (req, res) => {
     res.json(result);
   });
 };
-const addNewBooks= (req, res) => {
-  const { img_book,title, description , publisher,edition,pages , price,author } = req.body;
+const addNewBooks = (req, res) => {
+  const {
+    img_book,
+    title,
+    description,
+    publisher,
+    edition,
+    pages,
+    price,
+    author,
+  } = req.body;
   const query = `INSERT INTO book (mg_book,title, description , publisher,edition,pages , price,author) VALUES (?, ?, ?, ?,?, ?, ?, ?)`;
-  const data = [img_book,title, description , publisher,edition,pages , price,author];
+  const data = [
+    img_book,
+    title,
+    description,
+    publisher,
+    edition,
+    pages,
+    price,
+    author,
+  ];
   db.query(query, data, (err, results) => {
     if (err) {
       throw err;
@@ -69,11 +59,30 @@ const addNewBooks= (req, res) => {
   });
 };
 
-const updateBooksByID =(req, res) => {
+const updateBooksByID = (req, res) => {
   const id = req.params.book_id;
-  const { img_book,title, description , publisher,edition,pages , price,author } = req.body;
+  const {
+    img_book,
+    title,
+    description,
+    publisher,
+    edition,
+    pages,
+    price,
+    author,
+  } = req.body;
   const query = `UPDATE book SET img_book=?,title=?, description=? , publisher=?,edition=?,pages=? , price=?,author=? WHERE id=${id};`;
-  const data = [img_book,title, description , publisher,edition,pages , price,author,id];
+  const data = [
+    img_book,
+    title,
+    description,
+    publisher,
+    edition,
+    pages,
+    price,
+    author,
+    id,
+  ];
   db.query(query, data, (err, result) => {
     if (err) throw err;
     console.log("RESULT: ", result);
@@ -85,5 +94,5 @@ module.exports = {
   getBooksByCategory,
   deleteBooksByID,
   addNewBooks,
-  updateBooksByID
+  updateBooksByID,
 };
