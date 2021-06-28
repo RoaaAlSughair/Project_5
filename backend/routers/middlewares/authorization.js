7585afef2207949a6db409de4efc15c3df00fc7e
Heba_Alshiyyab <81535776+HebaShiyyab@users.noreply.github.com>
2021-06-28 15:57:20 +0300

    authorization is done

--- backend/routers/middlewares/authorization.js
+++ backend/routers/middlewares/authorization.js
@@ -1,17 +1,16 @@
 const connection = require("./../../db/db");
 
-const authorization = () => {
+const authorization = (token) => {
   return async (req, res, next) => {
-    const id = req.params.user_id;
-    const query = `SELECT* FROM users WHERE user_id=?`;
-    const data = [id];
+      const role_id = req.token.role_id;
+    const query = `SELECT roles FROM role WHERE role_id=?`;
+    const data = [role_id];
     const user = await connection.query(query,data, (err, result) => {
       if (err) {
         res.status(400).json(err);
       }
-      res.status(201).json(result);
+    
     });
-
     if (!user) return res.status(401).json({ message: "forbidden" });
 
     req.user = user.get();
