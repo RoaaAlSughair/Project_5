8e9b19b5a3977d84d84d618b5c337ab6ce77fc47
C05_Basem <basemhmoud735@gmail.com>
2021-06-28 19:30:17 +0300

    delete books

--- backend/routers/routes/books.js
+++ backend/routers/routes/books.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const bookRouter = express.Router();
-const { getAllBooks, getBooksByCategory } = require("./../controllers/book");
+const { getAllBooks, getBooksByCategory,deleteBooksByID } = require("./../controllers/book");
 
 bookRouter.get("/", getAllBooks);
 bookRouter.get("/Category", getBooksByCategory);
+bookRouter.delete("/:id", deleteBooksByID);
 module.exports = bookRouter;
