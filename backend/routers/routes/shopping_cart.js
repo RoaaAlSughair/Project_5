47dfcb1c68b67ec462c2d265aaac73b6d2f7b2ab
RuaaAlsugheir <alsugheirruaa@gmail.com>
2021-06-28 22:46:53 +0300

    updated shopping cart route with middlewares

--- backend/routers/routes/shopping_cart.js
+++ backend/routers/routes/shopping_cart.js
@@ -1,5 +1,9 @@
 const express = require("express");
 
+// import middleware
+const authentication = require("../middlewares/authentication");
+const authorization = require("../middlewares/authorization");
+
 // Import functions
 const { addToShoppingCart, removeFromShoppingCart } = require("../controllers/shopping_cart");
 
@@ -7,20 +11,8 @@ const { addToShoppingCart, removeFromShoppingCart } = require("../controllers/sh
 const shoppingCartRouter = express.Router();
 
 // Connect function to router
-shoppingCartRouter.post("/cart", addToShoppingCart);
-shoppingCartRouter.delete("/cart/:book_id", removeFromShoppingCart);
+shoppingCartRouter.post("/cart", authorization, authorization("user"), addToShoppingCart);
+shoppingCartRouter.delete("/cart/:book_id" , authentication, authorization("user"), removeFromShoppingCart);
 
 // Export router
 module.exports = shoppingCartRouter;
-
-// to be used in server.js:
-/*
-in requirements:
-require("dotenv").config();
-
-in routers:
-const shoppingCartRouter = require("./routers/routes/shopping_cart");
-
-in app routers:
-app.use(shoppingCartRouter);
-*/
