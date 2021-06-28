5d785fa32571c118d774b32b2026dcba586bdc26
RuaaAlsugheir <alsugheirruaa@gmail.com>
2021-06-28 21:11:31 +0300

    All book functions are in one file

--- backend/routers/controllers/books.js
+++ backend/routers/controllers/books.js
@@ -1,11 +1,48 @@
-const db = require("./../../db/db");
+const connection = require("./../../db/db");
+
 const getAllBooks = (req, res) => {
   const query = `SELECT * FROM book`;
-  db.query(query, (err, results) => {
+
+  connection.query(query, (err, result) => {
     if (err) throw err;
-    res.status(201).json(results);
+    
+    res.status(201).json(result);
   });
 };
+
+const getBookByTitle = (req, res) => {
+  // get the book's title
+  const title = req.query.title;
+
+  // inject the title in the query order to get the result
+  const data = [title];
+  const query = `SELECT * FROM book WHERE title = ?;`;
+  connection.query(query, data, (err, result) => {
+    if (err) {
+      throw err;
+    }
+    res.status(200).json(result);
+  });
+};
+
+const getBooksByAuthor = (req, res) => {
+  // get the author
+  const author = req.query.author;
+
+  // inject the author in the query order to get the result
+  const data = [author];
+  const query = `SELECT * FROM book WHERE author = ?;`;
+  connection.query(query, data, (err, result) => {
+    if (err) {
+      throw err;
+    }
+    res.status(200).json(result);
+  });
+};
+
+// export functions
 module.exports = {
   getAllBooks,
+  getBookByTitle,
+  getBooksByAuthor,
 };
--- backend/routers/routes/books.js
+++ backend/routers/routes/books.js
@@ -1,7 +1,15 @@
 const express = require("express");
+
 const bookRouter = express.Router();
-const { getAllBooks } = require("./../controllers/book");
+
+const {
+  getAllBooks,
+  getBookByTitle,
+  getBooksByAuthor,
+} = require("./../controllers/books");
 
 bookRouter.get("/", getAllBooks);
+bookRouter.get("/books", getBookByTitle);
+bookRouter.get("/books", getBooksByAuthor);
 
 module.exports = bookRouter;
