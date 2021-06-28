6c0b16015d2cba06c2c07cec389cbb2cf414ade6
RuaaAlsugheir <alsugheirruaa@gmail.com>
2021-06-28 22:35:01 +0300

    added middlewares

--- backend/routers/routes/books.js
+++ backend/routers/routes/books.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const bookRouter = express.Router();
+
+const authentication = require("../middlewares/authentication");
+const authorization = require("../middlewares/authorization");
+
 const {
   getAllBooks,
   getBooksByCategory,
@@ -12,10 +16,10 @@ const {
 
 bookRouter.get("/", getAllBooks);
 bookRouter.get("/Category", getBooksByCategory);
-bookRouter.get("/books", getBookByTitle);
-bookRouter.get("/books", getBooksByAuthor);
-bookRouter.delete("/:id", deleteBooksByID);
-bookRouter.post("/", addNewBooks);
-bookRouter.put("/:id", updateBooksByID);
+bookRouter.get("/search_title", getBookByTitle);
+bookRouter.get("/search_author", getBooksByAuthor);
+bookRouter.delete("/:id", authentication, authorization("admin"), deleteBooksByID);
+bookRouter.post("/", authentication, authorization("admin"), addNewBooks);
+bookRouter.put("/:id", authentication, authorization("admin"), updateBooksByID);
 
 module.exports = bookRouter;
