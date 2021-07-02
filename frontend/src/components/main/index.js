bd5709fefe53af5d50496794d9bdf978b9964175
RuaaAlsugheir <alsugheirruaa@gmail.com>
2021-07-02 22:50:16 +0300

    Main component is now working with redux

--- frontend/src/components/main/index.js
+++ frontend/src/components/main/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Axios from "axios";
 import { useDispatch, useSelector } from "react-redux";
 import { setBooks } from "../../reducers/main";
@@ -13,25 +13,43 @@ const Main = () => {
   });
 
   useEffect(() => {
-    Axios.get("http://localhost:5000/books/")
+    Axios.get("http://localhost:5000/book/")
       .then((res) => {
         dispatch(setBooks(res.data));
-		res.data.map((elem, index) => {
-			return <div key={index} className="book">
-				{/* <img className="book_element" src={elem.book_img} alt="An image of a book cover"/> */}
-				<p className="book_element">{elem.title}</p>
-				<p className="book_element">{elem.description}</p>
-				<p className="book_element">{elem.author}</p>
-				{/* <p className="book_element">{elem.ratings}</p> */}
-			</div>
-		})
       })
       .catch((err) => {
         throw err;
       });
-  }, []);
+  });
 
-  return <div className="Main"></div>;
+  return (
+    <div className="Main">
+      {state.books.map((elem, index) => {
+        return (
+          <div key={index} className="book">
+            <img className="book_element" src={elem.book_img} alt="Book cover"/>
+            <p className="book_element">{elem.title}</p>
+            <p className="book_element">{elem.description}</p>
+            <p className="book_element">{elem.author}</p>
+            <p className="book_element">{elem.price}</p>
+            {/* <p className="book_element">{elem.ratings}</p> */}
+          </div>
+        );
+      })};
+    </div>
+  );
 };
 
 export default Main;
+
+/*
+Add in "./src/reducers/index" :
+import main from "../reducers/main" and use it
+
+Add in "./src/App" :
+import Main from "./components/main/index" and use it
+
+Add in "./src/index" :
+import { Provider } from "react-redux"; and use it (don't forget to delete "import { BrowserRouter as Router } from 'react-router-dom';")
+import store from "./reducers/index"; and use it
+*/
