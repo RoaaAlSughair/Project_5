57d606f4182bcd88345a40a7ce4febf6c4cb415d
RuaaAlsugheir <alsugheirruaa@gmail.com>
2021-07-02 22:41:05 +0300

    Nav bar component initial stage

--- frontend/src/components/navigation/index.js
+++ frontend/src/components/navigation/index.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const Navigation = () => {
-	return <div className="App">
-		Navigation component
+
+	const [search, setSearch] = useState("");
+	const handleChange = (e) => {
+		setSearch(e.target.value);
+	}
+
+	return <div className="NavBar">
+		<Link to="/categories">Categories</Link>
+		<Link to="/authors">Authors</Link>
+		<input type="text" placeholder="Search book by title or author" onChange={handleChange}/>
+		<span><button>Search</button></span>
+		<Link to="/login">Login</Link>
+		<Link to="/Register"><button>Sign up</button></Link>
 		</div>;
 };
 
