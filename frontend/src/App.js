2603aa5b4598b912e66e086258432656b433c028
Ruaa_Alsugheir <81527046+RuaaAlsugheir@users.noreply.github.com>
2021-07-03 21:49:33 +0300

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,8 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
-import BookPage from './components/bookPage';
-import Footer from "./components/footer/index"
 
 const App = () => {
-	return <div className="App">
-	{/* <Footer/> */}
-	<BookPage/>
-	</div>;
+return <div className="App">App component</div>;
 };
 
 export default App;
