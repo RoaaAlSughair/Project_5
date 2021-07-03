7ee1c99715afdb498c34b4fdada8256720554a92
Heba_Alshiyyab <81535776+HebaShiyyab@users.noreply.github.com>
2021-07-03 21:26:44 +0300

    AboutUs

---
+++ frontend/src/components/footer/AboutUs.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './footer.css';
+
+
+export default function AboutUs() {
+  return (
+    <div className="About">
+      <h1> About  </h1>
+      <ion-icon name="mail-outline">booksworms@yahoo.com</ion-icon><br></br>
+      <ion-icon name="call-outline">00962-798-777-222</ion-icon><br></br>
+      <ion-icon name="location-outline">Jordan-Amman-abdullah ghosha street-abdullah bilding</ion-icon>
+     
+    </div>
+  );
+}
