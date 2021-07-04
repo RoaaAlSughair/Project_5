dc1d165d3721bcdc0c7abf04cb44359d703b3c03
C05_Basem <basemhmoud735@gmail.com>
2021-07-04 19:52:39 +0300

    finish login page in front end

--- frontend/src/components/auth/login/index.js
+++ frontend/src/components/auth/login/index.js
@@ -1,1 +1,90 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import { useDispatch, useSelector } from "react-redux";
+import { setToken } from "../../../reducers/login";
+import loginImg from "./Trip-cuate.png";
+import "./login.css";
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [message, setMessage] = useState("");
+  const dispatch = useDispatch();
+  const signIn = () => {
+    axios
+      .post("http://localhost:5000/login", { email, password })
+      .then((result) => {
+        console.log(result);
+        setMessage("Login Successful");
+        dispatch(setToken(result.data.token));
+      })
+      .catch((err) => {
+        setMessage(err.response.data);
+      });
+  };
+  return (
+    <div className="container">
+      <div className="auth">
+        <div className="leftAuth">
+          <img src={loginImg} alt="" />
+        </div>
+        <div className="rightAuth">
+          <div>
+            <h1>Welcome, Login</h1>
+          </div>
+          <table>
+            <tr>
+              <td>
+                <label>Email</label>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <input
+                  type="email"
+                  required
+                  placeholder="Email"
+                  onChange={(e) => {
+                    setEmail(e.target.value);
+                  }}
+                />
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <label>Password</label>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <input
+                  type="password"
+                  required
+                  placeholder="Password"
+                  onChange={(e) => {
+                    setPassword(e.target.value);
+                  }}
+                />
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <button onClick={signIn}>Login</button>
+              </td>
+            </tr>
+          </table>
+          <p>{message}</p>
+          <p>
+            Don't have account?{" "}
+            <Link to="/register" className="authLink">
+              Register
+            </Link>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
