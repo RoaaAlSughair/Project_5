ca028477822f63ba25a76146b18b245900697246
RuaaAlsugheir <alsugheirruaa@gmail.com>
2021-07-06 20:32:43 +0300

    book details initial code

---
+++ frontend/src/components/book_details/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function BookDetails() {
+    return (
+        <div className="BookDetails">
+            <img className="Cover" />
+            <p className="Title"></p>
+            <p className="Author"></p>
+            <p className="Description"></p>
+            <p className="Price"></p>
+            <div className="Rating"></div>
+
+            {/* <Comments /> */}
+        </div>
+    )
+}
