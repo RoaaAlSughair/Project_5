ec80ce811c9eb08c27c253686580dafe7ed75063
RuaaAlsugheir <alsugheirruaa@gmail.com>
2021-07-17 01:43:53 +0300

    fixing refresh issue test 2

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.use(ContactUsRouter);
 app.use(CommentsRouter);
 app.use("/category", categoryRouter);
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server On ${PORT}`);
 });
